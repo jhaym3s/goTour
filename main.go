0aa4f20a7fa8210fc5e706a8a86448376f7e61de
Jhaym3s <jhaymescode@gmail.com>
2023-02-25 17:08:43 +0100

    interface

--- main.go
+++ main.go
@@ -8,6 +8,17 @@ import (
 type Abser interface {
 	Abs() float64
 }
+//type switch cases 
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
 
 func main() {
 	var a Abser
@@ -22,6 +33,21 @@ func main() {
 	//a = v
 
 	fmt.Println(a.Abs())
+
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	//If i holds a T, then t will be the underlying value and ok will be true.
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	w, ok := i.(float64)
+	fmt.Println(w, ok)
+
+	w = i.(float64) // panic
+	fmt.Println(w)
 }
 
 type MyFloat float64
